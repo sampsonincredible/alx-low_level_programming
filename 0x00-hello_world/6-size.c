625888263b5f147bcebbf9bb8d8ac2a12c2858d1
Samuel Sampson <samuelsampson86@gmail.com>
2023-07-13 05:43:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various types on the computer
- * Return 0 (Success)
+ * Return:0 (Success)
  */
 int main(void)
 {
@@ -17,4 +17,3 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
-
