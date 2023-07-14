65aa5290521cea2b6a5ace24c95f2c502cfe46e9
Samuel Sampson <samuelsampson86@gmail.com>
2023-07-14 11:00:29 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints all sigles numbers of base 10 starting from 0 no char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
