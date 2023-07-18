ac4400aa95924b09f17430eb93435e2bf543efb2
Samuel Sampson <samuelsampson86@gmail.com>
2023-07-18 02:21:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print all alphabet in lowercase
+ */
+
+void main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
