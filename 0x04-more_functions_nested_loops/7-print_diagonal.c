78162c513a7701e9527bb79bc60f3f5998f2cdfc
Samuel Sampson <samuelsampson86@gmail.com>
2023-07-20 02:31:29 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: numbers of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	if (n <=)
+	{
